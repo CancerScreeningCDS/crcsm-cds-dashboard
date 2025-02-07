431276833491c06f897c5d344e10d56f0ccbcb0b
Jaehoon Lee <jaehoonlee@mitre.org>
2025-02-07 13:10:12 -0500

    no cert key on client side

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,15 +1,8 @@
 // Use a custom logger application
-const LOGGER_HOST = process.env?.REACT_APP_LOGGER_HOST || "https://localhost"
+const LOGGER_HOST = process.env?.REACT_APP_LOGGER_HOST || "http://localhost"
 const LOGGER_PORT = process.env?.REACT_APP_LOGGER_PORT || "4040";
 const logger_url = `${LOGGER_HOST}:${LOGGER_PORT}/log`;
 console.log("Logging at", logger_url);
-
-const certPath = 'server.cert';
-const keyPath = 'server.key';
-
-const cert = fs.readFileSync(certPath);
-const key = fs.readFileSync(keyPath);
-console.log("Loaded certificate: ", certPath);
 
 export async function logMsg(msg) {
   try {
@@ -17,13 +10,10 @@ export async function logMsg(msg) {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Content-Length': msg.length
+        'Content-Type': 'application/json'
       },
       mode:'cors',
-      body: JSON.stringify(msg),
-      cert: cert,
-      key: key
+      body: JSON.stringify(msg)
     }).then(response => {
               let cachedPostData = response.json();
     });
